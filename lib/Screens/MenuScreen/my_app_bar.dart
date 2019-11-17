f9d58e3c85beab2b5d57243a1e5d5f7f5837b2bc
HUSAM NASRALLAH <husamnasrallah@HUSAMs-iMac-Pro.local>
2019-11-17 19:17:32 +0300

    1

--- lib/Screens/MenuScreen/my_app_bar.dart
+++ lib/Screens/MenuScreen/my_app_bar.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:ozone_managment/Screens/CrudScreens/adddata.dart';
 
-class MYAppBar extends StatelessWidget {
+class MYAppBar extends StatefulWidget {
   const MYAppBar({Key key}) : super(key: key);
 
+  @override
+  _MYAppBarState createState() => _MYAppBarState();
+}
+
+class _MYAppBarState extends State<MYAppBar> {
   @override
   Widget build(BuildContext context) {
     return           AppBar(
@@ -15,30 +20,38 @@ class MYAppBar extends StatelessWidget {
             actions: <Widget>[
               Padding(
                 padding: const EdgeInsets.only(left: 15),
-                child: GestureDetector(
-                  child: Row(
+                child: Row(
                     children: <Widget>[
-                      Icon(
-                        Icons.add,
-                        color: Colors.yellow,
-                        size: 25,
+                      InkWell(
+                                              child: Icon(
+                          Icons.add,
+                          color: Colors.yellow,
+                          size: 25,
+                        ),
+                        onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context) => AddData(),
+                  )),
                       ),
                       SizedBox(
                         width: 20,
                       ),
-                      Icon(
-                        Icons.filter_list,
-                        color: Colors.yellow,
-                        size: 25,
+                      InkWell(
+                                              child: Icon(
+                          Icons.refresh,
+                          color: Colors.yellow,
+                          size: 25,
+                        ),
+                        onTap: () => null,
                       ),
+                      
                     ],
                   ),
-                  onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => AddData(),
-                  )),
-                ),
+                  
+               
               )
             ],
           );
   }
+
+
 }
