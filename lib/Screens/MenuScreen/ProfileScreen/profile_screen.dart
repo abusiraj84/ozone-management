2f2728405876c8f0975d4a468fb6b671e825612f
HUSAM NASRALLAH <husamnasrallah@HUSAMs-iMac-Pro.local>
2019-11-22 12:31:19 +0300

    1

--- lib/Screens/MenuScreen/ProfileScreen/profile_screen.dart
+++ lib/Screens/MenuScreen/ProfileScreen/profile_screen.dart
@@ -87,165 +87,193 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: <Widget>[
-          Stack(
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage( 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTDh6FxmYhUI0Fnem-zHXIW8dQzmCTZA7qvjkZvJRUGBPuiPiUm') ,
+                       colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
+                       fit: BoxFit.fill
+                  )
+                ),
+              ),
+          ),
+          Column(
             children: <Widget>[
-              Container(
-                // color: Colors.amber,
-                height: 320,
-                child: Stack(
-                  children: <Widget>[
-                    Container(
-                        height: 200,
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                          image: NetworkImage(
-                            _userImg,
-                          ),
-                          fit: BoxFit.cover,
-                        )),
-                        child: Container(
+              Stack(
+                children: <Widget>[
+                  Container(
+                    // color: Colors.amber,
+                    height: 320,
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                            height: 200,
                             decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                                colors: [
-                                  Colors.black.withOpacity(1),
-                                  Colors.black.withOpacity(0.6),
-                                ],
+                                image: DecorationImage(
+                              image: NetworkImage(
+                                _userImg,
+                              ),
+                              fit: BoxFit.cover,
+                            )),
+                            child: Container(
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                    colors: [
+                                      Colors.black.withOpacity(1),
+                                      Colors.black.withOpacity(0.6),
+                                    ],
+                                  ),
+                                ),
+                                child: Container(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 50, right: 20, left: 20),
+                                    child: FadeAnimation(
+                                        1,
+                                        Row(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: <Widget>[
+                                            Container(
+                                              height: 35,
+                                              width: 35,
+                                              decoration: BoxDecoration(
+                                                  shape: BoxShape.circle,
+                                                  border: Border.all(
+                                                      color: Colors.white)),
+                                              child: Icon(
+                                                Icons.menu,
+                                                color: Colors.white,
+                                                size: 15,
+                                              ),
+                                            ),
+                                            Expanded(
+                                              child: Column(
+                                                children: <Widget>[
+                                                  SizedBox(
+                                                    height: 10,
+                                                  ),
+                                                  Text(
+                                                    'الملف الشخصي',
+                                                    style: TextStyle(
+                                                        color: Colors.white,
+                                                        fontSize: 18,
+                                                        fontWeight:
+                                                            FontWeight.bold),
+                                                    textAlign: TextAlign.center,
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                            Container(
+                                                height: 35,
+                                                width: 35,
+                                                decoration: BoxDecoration(
+                                                    shape: BoxShape.circle,
+                                                    border: Border.all(
+                                                        color: Colors.white)),
+                                                child: IconButton(
+                                                  icon: Icon(
+                                                    Icons.arrow_forward,
+                                                    color: Colors.white,
+                                                    size: 15,
+                                                  ),
+                                                  onPressed: () =>
+                                                      Navigator.pop(context),
+                                                )),
+                                          ],
+                                        )),
+                                  ),
+                                ))),
+                        Positioned(
+                          bottom: 70,
+                          right: MediaQuery.of(context).size.width / 2 - 80,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border:
+                                    Border.all(color: Colors.white, width: 6)),
+                            child: Hero(
+                              tag: _email,
+                              child: CircleAvatar(
+                                backgroundImage: NetworkImage(_userImg),
+                                radius: 70,
                               ),
                             ),
-                            child: Container(
-                              child: Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 50, right: 20, left: 20),
-                                child:FadeAnimation(
-            1, Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    Container(
-                                      height: 35,
-                                      width: 35,
-                                      decoration: BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          border:
-                                              Border.all(color: Colors.white)),
-                                      child: Icon(
-                                        Icons.menu,
-                                        color: Colors.white,
-                                        size: 15,
-                                      ),
-                                    ),
-                                    Expanded(
-                                      child: Column(
-                                        children: <Widget>[
-                                          SizedBox(
-                                            height: 10,
-                                          ),
-                                          Text(
-                                            'الملف الشخصي',
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 18,
-                                                fontWeight: FontWeight.bold),
-                                            textAlign: TextAlign.center,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    Container(
-                                        height: 35,
-                                        width: 35,
-                                        decoration: BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            border: Border.all(
-                                                color: Colors.white)),
-                                        child: IconButton(
-                                          icon: Icon(
-                                            Icons.arrow_forward,
-                                            color: Colors.white,
-                                            size: 15,
-                                          ),
-                                          onPressed: () =>
-                                              Navigator.pop(context),
-                                        )),
-                                  ],
-                                )),
-                              ),
-                            ))),
-                    Positioned(
-                      bottom: 70,
-                      right: MediaQuery.of(context).size.width / 2 - 80,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(color: Colors.white, width: 6)),
-                        child: Hero(
-                          tag: _email,
-                          child: CircleAvatar(
-                            backgroundImage: NetworkImage(_userImg),
-                            radius: 70,
                           ),
                         ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 25,
-                      width: MediaQuery.of(context).size.width,
-                      child: FadeAnimation(
-            0.3,Text(
-                        _name,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
+                        Positioned(
+                          bottom: 25,
+                          width: MediaQuery.of(context).size.width,
+                          child: FadeAnimation(
+                            0.3,
+                            Text(
+                              _name,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
                         ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),),
-                    Positioned(
-                      bottom: 5,
-                      width: MediaQuery.of(context).size.width,
-                      child: FadeAnimation(
-            0.5,Text(
-                        _email,
-                        style: TextStyle(
-                          color: Colors.grey.shade500,
-                          fontSize: 15,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),)
+                        Positioned(
+                            bottom: 5,
+                            width: MediaQuery.of(context).size.width,
+                            child: FadeAnimation(
+                              0.5,
+                              Text(
+                                _email,
+                                style: TextStyle(
+                                  color: Colors.grey.shade500,
+                                  fontSize: 15,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            )),
+                      ],
                     ),
-                  ],
+                  ),
+                ],
+              ),
+              Divider(),
+              SizedBox(
+                height: 20,
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text('تعديل الملف'),
+                color: Colors.amber,
+              ),
+              FlatButton(
+                onPressed: () async {
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  prefs.remove('email');
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext ctx) => LoginScreen()));
+                },
+                child: Text(
+                  'تسجيل خروج',
+                  style: TextStyle(color: Colors.white),
                 ),
+                color: Colors.black,
               ),
             ],
           ),
-          Divider(),
-          SizedBox(
-            height: 20,
-          ),
-          FlatButton(
-            onPressed: () {},
-            child: Text('تعديل الملف'),
-            color: Colors.amber,
-          ),
-          FlatButton(
-            onPressed: () async {
-              SharedPreferences prefs = await SharedPreferences.getInstance();
-              prefs.remove('email');
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (BuildContext ctx) => LoginScreen()));
-            },
-            child: Text(
-              'تسجيل خروج',
-              style: TextStyle(color: Colors.white),
-            ),
-            color: Colors.black,
-          )
         ],
       ),
     );
